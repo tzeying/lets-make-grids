3d9580e91ca53ed1b8139d1ab2defde3a5fbedde
Tzeying <tzeyingcheng@gmail.com>
2022-12-26 15:23:50 +0800

    generate different grid sizes

--- grids-grids-grids/pages/grid.js
+++ grids-grids-grids/pages/grid.js
@@ -1,40 +1,61 @@
 import { Rect, SVG } from '@svgdotjs/svg.js';
 import { useEffect } from 'react';
 
-let w = 300; let h = 300; let pointSize = 5;
-const drawLineGrid = (width, height, draw) => {
-    for (var x = 0; x <= width; x += 20) {
+
+const drawLineGrid = (width, height, draw, scale) => {
+    for (var x = 0; x <= width; x += scale) {
         draw.line(x, 0, x, height).stroke({ width: 0.4, color: 'rgba(0,65,255,0.7)' })
     }
-    for (var y = 0; y <= height; y += 20) {
+    for (var y = 0; y <= height; y += scale) {
         draw.line(0, y, width, y).stroke({ width: 0.4, color: 'rgba(0,65,255,0.7)' })
     }
 }
 
-const drawDotGrid = (draw, width = w, height = h, color = '#E1E1E1') => {
-    for (var x = 0; x <= width; x += 20 * 2.5) {
-        for (var y = 0; y <= height; y += 20 * 2.5) {
+const drawDotGrid = (draw, scale, dotGridScaleMultiplier, width = w, height = h, color = '#E1E1E1') => {
+    for (var x = 0; x <= width; x += scale * dotGridScaleMultiplier) {
+        for (var y = 0; y <= height; y += scale * dotGridScaleMultiplier) {
             draw.rect(pointSize, pointSize).move(x - (pointSize / 2), y - (pointSize / 2)).fill(color)
         }
     }
 }
 
-const highlightArea = (width, height, draw) => {
+const highlightArea = (width, height, draw, scale, dotGridScaleMultiplier) => {
     draw.rect(width, height).fill('rgba(0,65,255,0.1)').stroke('rgba(0,65,255,0.2)');
-    drawLineGrid(width, height, draw);
-    drawDotGrid(draw, width, height, '#0041FF');
+    drawLineGrid(width, height, draw, scale);
+    drawDotGrid(draw, scale, dotGridScaleMultiplier, width, height, '#0041FF');
 }
 
-export default function () {
+const plot = (size) => {
+    let { w, h } = size;
+    let sqft = w * h;
+    let step = [20, 10, 8]
+    if (sqft < 100) {
+        return { scale: step[0], multiplier: 2.5, plotW: w * step[0], plotH: h * step[0] }
+    } else if (sqft >= 100 && sqft < 250) {
+        return { scale: step[1], multiplier: 5, plotW: w * step[1], plotH: h * step[1] }
+    } else if (sqft >= 250) {
+        return { scale: step[2], multiplier: 7.5, plotW: w * step[2], plotH: h * step[2] }
+    }
+}
+
+let w = 270; let h = 270; let pointSize = 5;
+
+// let size = { w: 30, h: 15 };
+export default function ({ size, index }) {
+    let { scale, multiplier, plotW, plotH } = plot(size);
+
     useEffect(() => {
-        let draw = SVG().addTo('#draw').size(w, h);
+        let draw = SVG().addTo(`#draw${index}`).size(w, h);
         let group = draw.group();
-        drawDotGrid(group);
-        highlightArea(100, 100, group);
+        drawDotGrid(group, scale, multiplier);
+        highlightArea(plotW, plotH, group, scale, multiplier);
         group.move(4, 4);
     }, [])
 
     return (
-        <div id="draw"></div>
+        <div className="flex border border-slate-300 rounded p-4 flex-col">
+            <p className='text-lg font-medium pb-2'>{size.w} x {size.h}</p>
+            <div id={`draw${index}`}></div>
+        </div>
     );
 }
